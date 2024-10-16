95f14500ffd7eb45e1bc052f36305a41787673a7
ctjioman <chris_tjioman@trendmicro.com>
2024-10-16 15:19:42 +1100

    Q3

--- codingtests/be-test/src/getPayment.ts
+++ codingtests/be-test/src/getPayment.ts
@@ -5,5 +5,9 @@ import { buildResponse } from './lib/apigateway';
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const paymentId = event.pathParameters?.id as string;
     const payment = await getPayment(paymentId);
-    return buildResponse(200, payment || {});
+
+    if (!payment) {
+        return buildResponse(404, {});
+    }
+    return buildResponse(200, payment);
 };
--- codingtests/be-test/test/getPayment.test.ts
+++ codingtests/be-test/test/getPayment.test.ts
@@ -19,10 +19,26 @@ describe('When the user requests the records for a specific payment', () => {
             },
         } as unknown as APIGatewayProxyEvent);
 
+        expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
         expect(result.statusCode).toBe(200);
         expect(JSON.parse(result.body)).toEqual(mockPayment);
+    });
 
-        expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
+    describe('When the specific payment does not exist', () => {
+        it('Returns a 404 error.', async () => {
+            const paymentId = randomUUID();
+
+            const getPaymentMock = jest.spyOn(payments, 'getPayment').mockResolvedValueOnce(null);
+
+            const result = await handler({
+                pathParameters: {
+                    id: paymentId,
+                },
+            } as unknown as APIGatewayProxyEvent);
+
+            expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
+            expect(result.statusCode).toBe(404);
+        });
     });
 });
 
