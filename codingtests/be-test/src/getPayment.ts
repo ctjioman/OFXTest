d856171bcc5b95b39e150e30add213aeaf66f07c
ctjioman <chris_tjioman@trendmicro.com>
2024-10-16 14:38:58 +1100

    Q1

--- codingtests/be-test/src/getPayment.ts
+++ codingtests/be-test/src/getPayment.ts
@@ -1,5 +1,9 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { getPayment } from './lib/payments';
+import { buildResponse } from './lib/apigateway';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    throw Error('Implement me!');
+    const paymentId = event.pathParameters?.id as string;
+    const payment = await getPayment(paymentId);
+    return buildResponse(200, payment || {});
 };
