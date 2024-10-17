7d20f7b4224f924a41477c9595115ffc14c02a3d
ctjioman <chris_tjioman@trendmicro.com>
2024-10-17 13:26:11 +1100

    Q5

--- codingtests/be-test/src/lib/payments.ts
+++ codingtests/be-test/src/lib/payments.ts
@@ -2,37 +2,48 @@ import { DocumentClient } from './dynamodb';
 import { GetCommand, PutCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
 
 export const getPayment = async (paymentId: string): Promise<Payment | null> => {
-    const result = await DocumentClient.send(
-        new GetCommand({
-            TableName: 'Payments',
-            Key: { paymentId },
-        })
-    );
+	const result = await DocumentClient.send(
+		new GetCommand({
+			TableName: 'Payments',
+			Key: { paymentId },
+		})
+	);
 
-    return (result.Item as Payment) || null;
+	return (result.Item as Payment) || null;
 };
 
-export const listPayments = async (): Promise<Payment[]> => {
-    const result = await DocumentClient.send(
-        new ScanCommand({
-            TableName: 'Payments',
-        })
-    );
+export const listPayments = async (currency: string): Promise<Payment[]> => {
+	const scanCommand: {
+		TableName: string;
+		FilterExpression?: string;
+		ExpressionAttributeValues?: { [key: string]: any };
+	} = {
+		TableName: 'Payments',
+	};
 
-    return (result.Items as Payment[]) || [];
+	if (currency) {
+		scanCommand.FilterExpression = 'currency = :currency';
+		scanCommand.ExpressionAttributeValues = {
+			':currency': currency,
+		};
+	}
+
+	const result = await DocumentClient.send(new ScanCommand(scanCommand));
+
+	return (result.Items as Payment[]) || [];
 };
 
 export const createPayment = async (payment: Payment) => {
-    await DocumentClient.send(
-        new PutCommand({
-            TableName: 'Payments',
-            Item: payment,
-        })
-    );
+	await DocumentClient.send(
+		new PutCommand({
+			TableName: 'Payments',
+			Item: payment,
+		})
+	);
 };
 
 export type Payment = {
-    id: string;
-    amount: number;
-    currency: string;
+	id: string;
+	amount: number;
+	currency: string;
 };
--- codingtests/be-test/src/listPayments.ts
+++ codingtests/be-test/src/listPayments.ts
@@ -3,6 +3,7 @@ import { buildResponse } from './lib/apigateway';
 import { listPayments } from './lib/payments';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const payments = await listPayments();
-    return buildResponse(200, { data: payments });
+	const currency = event.pathParameters?.currency as string;
+	const payments = await listPayments(currency);
+	return buildResponse(200, { data: payments });
 };
