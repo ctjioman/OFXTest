9ab5542ae8b0b103dee5bcd0b3f16fa4472be30c
ctjioman <chris_tjioman@trendmicro.com>
2024-10-17 13:07:35 +1100

    Q4

--- codingtests/be-test/src/createPayment.ts
+++ codingtests/be-test/src/createPayment.ts
@@ -2,12 +2,26 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { buildResponse, parseInput } from './lib/apigateway';
 import { createPayment, Payment } from './lib/payments';
 import { randomUUID } from 'crypto';
+import { z } from 'zod';
 
+const schema = z
+	.object({
+		amount: z.number(),
+		currency: z.string(),
+	})
+	.strict();
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const payment = parseInput(event.body || '{}') as Payment;
-    payment.id = await randomUUID();
+	const eventBody = parseInput(event.body || '{}');
 
-    await createPayment(payment);
-    return buildResponse(201, { result: payment.id });
+	const schemaResults = schema.safeParse(eventBody);
+
+	if (!schemaResults.success) {
+		return buildResponse(422, {});
+	}
+	const payment = eventBody as Payment;
+	payment.id = await randomUUID();
+
+	await createPayment(payment);
+	return buildResponse(201, { result: payment.id });
 };
--- codingtests/be-test/test/createPayment.test.ts
+++ codingtests/be-test/test/createPayment.test.ts
@@ -1,34 +1,73 @@
-import * as payments from "../src/lib/payments";
-import { randomUUID } from "crypto";
+import * as payments from '../src/lib/payments';
+import { randomUUID } from 'crypto';
 // import path from 'path';
-import { handler } from "../src/createPayment";
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { handler } from '../src/createPayment';
+import { APIGatewayProxyEvent } from 'aws-lambda';
 
-jest.mock("crypto");
+jest.mock('crypto');
 
-describe("When the user creates a payment", () => {
-  it("Generates a UUID and Returns the paymentId.", async () => {
-    const mockPayment ={
-        id: "abd123",
-        currency: "AUD",
-        amount: 2000,
-      }
-    const mockUUIDValue = "mockUUID";
-    const createPaymentMock = jest.spyOn(payments, "createPayment").mockResolvedValue();
-    (randomUUID as jest.Mock).mockResolvedValue(mockUUIDValue);
+describe('When the user creates a payment', () => {
+	const mockPayment = {
+		currency: 'AUD',
+		amount: 2000,
+	};
+	const createPaymentMock = jest.spyOn(payments, 'createPayment');
+	it('Generates a UUID and Returns the paymentId.', async () => {
+		const mockUUIDValue = 'mockUUID';
+		createPaymentMock.mockResolvedValueOnce();
+		(randomUUID as jest.Mock).mockResolvedValueOnce(mockUUIDValue);
 
-    const result = await handler({
-      body: JSON.stringify(mockPayment),
-    } as unknown as APIGatewayProxyEvent);
+		const result = await handler({
+			body: JSON.stringify(mockPayment),
+		} as unknown as APIGatewayProxyEvent);
 
-    expect(randomUUID).toHaveBeenCalled();
-    expect(createPaymentMock).toHaveBeenCalledWith({...mockPayment, id: mockUUIDValue});
+		expect(randomUUID).toHaveBeenCalled();
+		expect(createPaymentMock).toHaveBeenCalledWith({
+			...mockPayment,
+			id: mockUUIDValue,
+		});
 
-    expect(result.statusCode).toBe(201);
-    expect(result.body).toBe(JSON.stringify({ result: mockUUIDValue }));
-  });
+		expect(result.statusCode).toBe(201);
+		expect(result.body).toBe(JSON.stringify({ result: mockUUIDValue }));
+	});
+
+	describe('When the user adds a new value to the event', () => {
+		it('Returns a 422 error.', async () => {
+			const invalidPayment = { ...mockPayment, newValue: 'test' };
+
+			const result = await handler({
+				body: JSON.stringify(invalidPayment),
+			} as unknown as APIGatewayProxyEvent);
+			expect(result.statusCode).toBe(422);
+			expect(createPaymentMock).not.toHaveBeenCalled();
+		});
+	});
+
+	describe('When user enters a invalidType in the amount attribute', () => {
+		it('Returns a 422 error.', async () => {
+			const invalidPayment = { ...mockPayment, amount: 'invalidType' };
+
+			const result = await handler({
+				body: JSON.stringify(invalidPayment),
+			} as unknown as APIGatewayProxyEvent);
+			expect(result.statusCode).toBe(422);
+			expect(createPaymentMock).not.toHaveBeenCalled();
+		});
+	});
+
+	describe('When user enters a invalidType in the currency attribute', () => {
+		it('Returns a 422 error.', async () => {
+			const invalidPayment = { ...mockPayment, currency: 1000.0 };
+
+			const result = await handler({
+				body: JSON.stringify(invalidPayment),
+			} as unknown as APIGatewayProxyEvent);
+			expect(result.statusCode).toBe(422);
+			expect(createPaymentMock).not.toHaveBeenCalled();
+		});
+	});
 });
 
 afterEach(() => {
-  jest.resetAllMocks();
+	jest.resetAllMocks();
 });
